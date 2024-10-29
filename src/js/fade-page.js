32849134bbcbb5cd7f4f1bf9c775bd0176cb0478
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2024-10-30 00:04:27 +0200

    js

---
+++ src/js/fade-page.js
@@ -0,0 +1,20 @@
+const navLinks = document.querySelectorAll('.nav__link')
+
+console.log(navLinks)
+
+navLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault()
+
+    document.body.classList.add('fade-out')
+
+    // Переходимо до нової сторінки через короткий час після зникнення
+    setTimeout(() => {
+      window.location.href = link.href
+    }, 500) // 500ms збігається з тривалістю анімації
+  })
+})
+
+window.addEventListener('load', () => {
+  document.body.classList.add('fade-in')
+})
