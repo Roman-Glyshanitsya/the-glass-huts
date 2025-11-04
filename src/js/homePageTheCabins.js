1b769e5e6fc7e470fe17b187aa168e013ef43321
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2025-11-04 23:51:35 +0200

    up

---
+++ src/js/homePageTheCabins.js
@@ -0,0 +1,30 @@
+import Swiper from 'swiper'
+import { EffectCube, Autoplay } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/effect-cube'
+
+const swiperElements = document.querySelectorAll('.swiper')
+
+const delays = [0, 600, 1200]
+
+swiperElements.forEach((el, index) => {
+  setTimeout(() => {
+    new Swiper(el, {
+      modules: [EffectCube, Autoplay],
+      effect: 'cube',
+      grabCursor: true,
+      loop: true,
+      cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+      },
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      speed: 1500,
+    })
+  }, delays[index])
+})
