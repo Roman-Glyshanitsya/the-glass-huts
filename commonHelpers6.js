e8df9d78996dd644b5cde71229fea920a844dfc5
Roman-Glyshanitsya <79175208+Roman-Glyshanitsya@users.noreply.github.com>
2025-11-05 23:23:23 +0000

    Deploying to gh-pages from @ Roman-Glyshanitsya/the-glass-huts@6f29816048b5bdfc6d2208b415568b458cf3a72b 🚀

---
+++ commonHelpers6.js
@@ -0,0 +1,2 @@
+import"./assets/pageTransition-c89fbad0.js";import{a as e,b as t,A as a,g as o}from"./assets/vendor-e89245a2.js";new e(".swiper",{modules:[t,a],effect:"fade",fadeEffect:{crossFade:!0},loop:!0,speed:2500,autoplay:{delay:2e3,disableOnInteraction:!1}});const i=o.timeline();i.fromTo(".hut__title",{y:100,opacity:0},{y:0,opacity:1,duration:1});
+//# sourceMappingURL=commonHelpers6.js.map
