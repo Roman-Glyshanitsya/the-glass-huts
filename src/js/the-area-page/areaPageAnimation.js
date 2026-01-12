4e7ed23d0f2d2c2f41fb3c439b94ea8a01f361b0
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2026-01-12 02:25:28 +0200

    up

--- src/js/the-area-page/areaPageAnimation.js
+++ src/js/the-area-page/areaPageAnimation.js
@@ -1,5 +1,7 @@
 import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
+gsap.registerPlugin(ScrollTrigger)
 const tl = gsap.timeline()
 
 // Hero
@@ -36,3 +38,53 @@ tl.fromTo(
       duration: 1,
     },
   )
+
+// Main Gallery
+const areaItems = document.querySelectorAll('.parallax-item')
+
+areaItems.forEach(item => {
+  const image = item.querySelector('.parallax-image')
+  const text = item.querySelector('.parallax-text')
+
+  gsap.fromTo(
+    image,
+    { yPercent: 20 },
+    {
+      yPercent: -20,
+      ease: 'none',
+      scrollTrigger: {
+        trigger: item,
+        start: 'top bottom',
+        end: 'bottom top',
+        scrub: true,
+      },
+    },
+  )
+
+  gsap.fromTo(
+    text,
+    { yPercent: -20 },
+    {
+      yPercent: 20,
+      ease: 'none',
+      scrollTrigger: {
+        trigger: item,
+        start: 'top bottom',
+        end: 'bottom top',
+        scrub: true,
+      },
+    },
+  )
+})
+
+// Activities Title
+gsap.from('.area__gallery__title', {
+  scrollTrigger: {
+    trigger: '.area__gallery',
+    start: '-10% center',
+    end: '+=300px',
+    scrub: true,
+  },
+  x: -100,
+  opacity: 0,
+})
